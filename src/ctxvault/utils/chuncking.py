f35707582bc2cda78cf14a4b06ac918edb56b195
Filippo Venturini <filippoventurini00@gmail.com>
2025-12-12 14:11:09 +0100

    feat(chunking): complete basic version

--- src/ctxvault/utils/chuncking.py
+++ src/ctxvault/utils/chuncking.py
@@ -1,6 +1,9 @@
 from typing import List
 
-def chunking(text:str, chunk_size: int = 700, overlap: int = 100)->List[str]:
+def chunking(text:str, chunk_size: int = 700, overlap: int = 20)->List[str]:
     text_splitted = text.split(" ")
-    chunks = [text_splitted[i:chunk_size]for i in range (0, len(text_splitted), chunk_size-overlap)]
+    chunks = [
+        " ".join(text_splitted[i:i+chunk_size]) 
+        for i in range(0, len(text_splitted), chunk_size - overlap)
+    ]
     return chunks
